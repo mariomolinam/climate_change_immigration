cc70d2a13ade1636a85a6b9fcf6622fab13d4711
Mario Molina <mm2535@cornell.edu>
2019-03-19 21:24:20 -0400

    change code to 1. create test set and 2. create dep var

--- python_code/create_subset.py
+++ python_code/create_subset.py
@@ -1,112 +1,190 @@
 import csv, random
 import pandas as pd
+import gc
 
 # Create training, validation, and test sets
 
 # path
-path_data = "/home/mario/Documents/environment_data"
+path_data = "/home/mario/Documents/environment_data/mmp_data"
 filename = "ind161_w_env.csv"
 full_path = path_data + '/' + filename
 
-# read file and count number of rows
-def row_counter(path):
-    # read in chunks of 1,000
-    mmp = pd.read_csv(path, chunksize=10000)
-    Nrows = 0
-    for line in mmp:
-        Nrows += line.shape[0]
-    return Nrows
+# load data
+d = pd.read_csv(full_path, dtype="str")
 
 
-# Define N for training, validation, and test set
-def N_for_sets(row_counter):
-    # get total number of rows
-    Nrows = row_counter(full_path)
-    prop = [0.5, 0.25, 0.25]
-    # Create training, validation, and test set
-    N_training = round(Nrows*prop[0])
-    N_validation = round(Nrows*prop[1])
-    N_test = round(Nrows*prop[2]) + 1 # add last row
-    # check numbers add up...
-    if(N_training + N_validation + N_test == Nrows):
-        print("Success!")
-        N_sets = { "N_training": N_training,
-                   "N_validation": N_validation,
-                   "N_test": N_test,
-                   "N_rows_total": Nrows}
-        return N_sets
-    else:
-        print("Numbers don't add up...")
-        return None
+def select_columns(data):
+    """
+        Input: pandas dataframe.
+        Task: select columns of dataframe and subset.
+
+        Return: pandas dataframe.
+    """
+    # define columns
+    cols = [
+            "commun", # community ID
+            "hhnum",  # household ID
+            "persnum", # person ID
+            "year",    # year within person
+            "geocode",
+            # "occur_5", # 5 occupational categories (not time-varying; based on survey year)
+            "usyr1", # first year of migration
+            "migf",
+            "age",
+            "mxmig", # whether they migrated in mexico until this year
+            "totmighh", # total number of prior U.S. migrants in the household (up until that year)
+            "lnvland_nr", # log(value of land) — excluding that bought by remittances
+            "tbuscat", # whether hh owns a business
+            "troom", # number of rooms in properties household owns
+            "agrim", # share of men working in agriculture in community
+            "minx2", # share of people earning twice the minimum wage or more
+            "dprev", # prevalence of migration in community (share of people who have ever migrated to the U.S. up until that year)
+            "metrocat", # metropolitan status of community
+            "ejido", # collective land system (0/1)
+            "lnpop", # log of population size in community
+            "primary", # if there is a school
+            "secondary", # if there is a secondary school
+            "bank",  # in community
+            "visaaccs", # visa accessibility to the U.S. in year
+            "infrate",  # inflation in Mexico in year
+            "mxminwag", # min wage in mexico in year
+            "mxunemp", # unemployment in Mexico
+            "usavwage", # U.S. average wages for low-skill work in year
+            "lntrade",  # log of trade between MX-U.S.
+            "logdist" # distance of community to the U.S.
+            ]
+    # define subset
+    subset = data[cols]
+    return subset
+
+# select columns and save as csv file
+subset = select_columns(d)
+subset.to_csv(path_data + "/ind161_w_env-subset.csv")
 
 
-N_sets = N_for_sets(row_counter)
 
-
-# create ids for training, validation, and test sets
-def create_sets_idx(N_sets, seed = 300):
-    # get all possible idx rows
-    Nrows = N_sets["N_rows_total"]
-    range_total = range(0, Nrows)
-    # TRAINING SET
-    # get ids for training_set
-    random.seed(seed) # set random seed
-    training_set_idx = random.sample(range_total, N_sets["N_training"])
-    # get ids that are left
-    range_left = set(range_total) - set(training_set_idx)
-    # VALIDATION SET
-    random.seed(seed) # set random seed
-    validation_set_idx = random.sample(range_left, N_sets["N_validation"])
-    # TEST SET
-    # it's defined by ids that are left
-    range_left = set(range_left) - set(validation_set_idx)
-    test_set_idx = list(range_left)
-    # output all sets
-    sets_idx = { "training_idx": training_set_idx,
-                 "validation_idx": validation_set_idx,
-                 "test_idx": test_set_idx }
-    return sets_idx
-
-
-# Create training, validation, and test sets
-def create_sets_files(create_sets_idx):
-    # get sets idx
-    sets_idx = create_sets_idx(N_sets)
-    header_original = list(pd.read_csv(full_path, nrows=1).columns)
-    # path to save data
-    path = "/home/mario/Documents/environment_data"
-    # loop through all sets
-    for key in sets_idx:
-        if key == "training_idx":
-            print("     Creating training set...")
-            filename = path + '/mmp_training_set.csv'
-            # skip validation and test
-            skip = set(sets_idx["validation_idx"]).union(set(sets_idx["test_idx"]))
-            skip = sorted(skip) # sort ids
-            mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
-            for chunk in mmp_piece:
-                chunk.to_csv(filename, mode="a", header=header_original)
-        elif key == "validation_idx":
-            print("     Creating validation set...")
-            filename = path + '/mmp_validation_set.csv'
-            # skip validation and test
-            skip = set(sets_idx["training_idx"]).union(set(sets_idx["test_idx"]))
-            skip = sorted(skip) # sort ids
-            mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
-            for chunk in mmp_piece:
-                chunk.to_csv(filename, mode="a", header=header_original)
-        else:
-            print("     Creating test set...")
-            filename = path + '/mmp_test_set.csv'
-            # skip validation and test
-            skip = set(sets_idx["validation_idx"]).union(set(sets_idx["training_idx"]))
-            skip = sorted(skip) # sort ids
-            mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
-            for chunk in mmp_piece:
-                chunk.to_csv(filename, mode="a", header=header_original)
-    # return message
-    message = "All files successfully created!"
+def create_test_set(data, seed=50):
+    # get unique users
+    users = subset.persnum.unique()
+    N_total = int(len(users) * 0.25)
+    random.seed(seed)
+    users_sub = random.sample(users, N_total)
+    data_test = data.loc[ data.persnum.isin(users_sub) ,:]
+    data_train = data.loc[ ~ data.persnum.isin(users_sub),: ]
+    # test set
+    file_test = "/ind161_test_set-DONOTUSE.csv"
+    data_test.to_csv(path_data + file_test)
+    # train set
+    file_train = "/ind161_train_set.csv"
+    data_train.to_csv(path_data + file_train)
+    # message
+    message = "Test and train sets created!"
     return message
 
-# Create training, validation, and test sets
-create_sets_files(create_sets_idx)
+# run functions
+create_test_set(subset)
+
+#
+#
+# # read file and count number of rows
+# def row_counter(path):
+#     # read in chunks of 1,000
+#     mmp = pd.read_csv(path, chunksize=10000)
+#     Nrows = 0
+#     for line in mmp:
+#         Nrows += line.shape[0]
+#     return Nrows
+#
+#
+# # Define N for training, validation, and test set
+# def N_for_sets(row_counter):
+#     # get total number of rows
+#     Nrows = row_counter(full_path)
+#     prop = [0.5, 0.25, 0.25]
+#     # Create training, validation, and test set
+#     N_training = round(Nrows*prop[0])
+#     N_validation = round(Nrows*prop[1])
+#     N_test = round(Nrows*prop[2]) + 1 # add last row
+#     # check numbers add up...
+#     if(N_training + N_validation + N_test == Nrows):
+#         print("Success!")
+#         N_sets = { "N_training": N_training,
+#                    "N_validation": N_validation,
+#                    "N_test": N_test,
+#                    "N_rows_total": Nrows}
+#         return N_sets
+#     else:
+#         print("Numbers don't add up...")
+#         return None
+#
+#
+# N_sets = N_for_sets(row_counter)
+#
+#
+# # create ids for training, validation, and test sets
+# def create_sets_idx(N_sets, seed = 300):
+#     # get all possible idx rows
+#     Nrows = N_sets["N_rows_total"]
+#     range_total = range(0, Nrows)
+#     # TRAINING SET
+#     # get ids for training_set
+#     random.seed(seed) # set random seed
+#     training_set_idx = random.sample(range_total, N_sets["N_training"])
+#     # get ids that are left
+#     range_left = set(range_total) - set(training_set_idx)
+#     # VALIDATION SET
+#     random.seed(seed) # set random seed
+#     validation_set_idx = random.sample(range_left, N_sets["N_validation"])
+#     # TEST SET
+#     # it's defined by ids that are left
+#     range_left = set(range_left) - set(validation_set_idx)
+#     test_set_idx = list(range_left)
+#     # output all sets
+#     sets_idx = { "training_idx": training_set_idx,
+#                  "validation_idx": validation_set_idx,
+#                  "test_idx": test_set_idx }
+#     return sets_idx
+#
+#
+# # Create training, validation, and test sets
+# def create_sets_files(create_sets_idx):
+#     # get sets idx
+#     sets_idx = create_sets_idx(N_sets)
+#     header_original = list(pd.read_csv(full_path, nrows=1).columns)
+#     # path to save data
+#     path = "/home/mario/Documents/environment_data"
+#     # loop through all sets
+#     for key in sets_idx:
+#         if key == "training_idx":
+#             print("     Creating training set...")
+#             filename = path + '/mmp_training_set.csv'
+#             # skip validation and test
+#             skip = set(sets_idx["validation_idx"]).union(set(sets_idx["test_idx"]))
+#             skip = sorted(skip) # sort ids
+#             mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
+#             for chunk in mmp_piece:
+#                 chunk.to_csv(filename, mode="a", header=header_original)
+#         elif key == "validation_idx":
+#             print("     Creating validation set...")
+#             filename = path + '/mmp_validation_set.csv'
+#             # skip validation and test
+#             skip = set(sets_idx["training_idx"]).union(set(sets_idx["test_idx"]))
+#             skip = sorted(skip) # sort ids
+#             mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
+#             for chunk in mmp_piece:
+#                 chunk.to_csv(filename, mode="a", header=header_original)
+#         else:
+#             print("     Creating test set...")
+#             filename = path + '/mmp_test_set.csv'
+#             # skip validation and test
+#             skip = set(sets_idx["validation_idx"]).union(set(sets_idx["training_idx"]))
+#             skip = sorted(skip) # sort ids
+#             mmp_piece = pd.read_csv(full_path, dtype="str", chunksize=10000, skiprows = skip, header=None)
+#             for chunk in mmp_piece:
+#                 chunk.to_csv(filename, mode="a", header=header_original)
+#     # return message
+#     message = "All files successfully created!"
+#     return message
+#
+# # Create training, validation, and test sets
+# create_sets_files(create_sets_idx)
--- python_code/random_forests.py
+++ python_code/random_forests.py
@@ -1,48 +1,83 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import numpy as np
 
+
 # paths
-path_data = "/home/mario/Documents/environment_data"
+path_data = "/home/mario/Documents/environment_data/mmp_data"
 path_git = "/home/mario/mm2535@cornell.edu/projects/ra_filiz/climate_change_immigration"
 
-# sets
-sets = { "training": "/mmp_training_set.csv",
-         "validation": "/mmp_validation_set.csv",
-         "test": "/mmp_test_set-DO-NOT-USE.csv" }
 
-#  V A R I A B L E   S E L E C T I O N
-socio_demo = [
-               "sex",
-               "age",
-               "marstat", # marital status
-               "occ",     # occupation at the time of survey
-               "hhincome" # household head income or wages
-              ]
-health = [
-            "kgs",     # weight
-            "health"   # health at age 14
-            "healthy"  # health at time of survey
-        ]
+# load data
+filename = path_data + "/ind161_train_set.csv"
+tr = pd.read_csv(filename, dtype="str")
+tr = tr.sort_values(by=["persnum", "year"])
+
+################################################################################
+###   V A R I A B L E   C R E A T I O N
+################################################################################
+
+# create DEPENDENT VARIABLE:
+#         - based on person-year (persnum-year) observation
+#         - keep individual history up to first migration,
+#               then drop the rest of person-year observations
+
+# remove all observation that has no valid value in usyr1
+idx_logical = ( (tr.usyr1 >= "1900") & (tr.usyr1 <= "2019") ) | (tr.usyr1 == '8888')
+tr = tr.loc[ idx_logical, :  ]
+
+# remove all observations whose "year" > "usyr1"
+idx_logical =  tr.year <= tr.usyr1 
+tr = tr.loc[idx_logical, ]
+
+# create first migration variable
+tr = tr.assign(first_mig = np.where(tr["usyr1"]=='8888', 0, 1) )
+
+tr[["persnum", "year", "usyr1", "first_mig"]].head(50)
+
+
+
+# load PRCP, TMAX, TMIN
+
+weather = ["prcp", "tmax", "tmin"]
+for item in weather:
+    filename = path_data + "/mmp_data/" + item + "_monthly_dev-norm_1980-2017.csv"
+    weather_mmp = pd.read_csv(filename, dtype="str")
+    # append to training dataset
+    tr = pd.merge(tr, weather_mmp, how="left", on="geocode")
+    break
+
 
 
 # D E P E N D E N T   V A R I A B L E
 # fix
-tr.loc[ tr.usyr1=="usyr1","usyr1"] = np.nan
-tr.usyr1 = tr["usyr1"].astype(float)
-# Migration equals 1 if first mgration to US took place, regardless of when it happened
-tr = tr.assign(first_migration = 0)
-tr.loc[ tr.usyr1.isnull() , "first_migration"] = np.nan
-tr.loc[ tr.usyr1.notnull() & ( (tr.usyr1 >= 1900) & (tr.usyr1 <= 2019) ) , "first_migration"] = 1
 
+tr_subset = tr.loc[:,["first_migration", "sex", "marstat"]]
+tr_subset.marstat[ (tr.marstat > '6') ] = np.nan
 
+tr_subset = tr_subset.dropna(axis=0, how="any")
+tr_subset = tr_subset.assign(female=pd.get_dummies(tr.sex).female)
+tr_subset[["Never married", "Married", "Consensual union", "Widowed", "Divorced", "Separated"]] = pd.get_dummies(tr_subset.marstat)
 
+# target vector
+y = np.array(tr_subset.first_migration)
+# features
+X = np.array(tr_subset.loc[:,["female", "Never married", "Married", "Consensual union", "Widowed", "Divorced", "Separated"]])
 
+# train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=200)
 
 #  R A N D O M   F O R E S T
-data_training = path_data + sets["training"]
-tr = pd.read_csv(data_training, dtype="str")
-header = list(tr.columns)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(X_train, y_train)
+
+# accuracy prediciton
+accuracy_score(y_train, rf.predict(X_train))
+accuracy_score(y_test, rf.predict(X_test))
+
 
 "mig" in tr.columns
 tr["migf"].value_counts(dropna=False)
