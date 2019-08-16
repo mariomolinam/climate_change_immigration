313cff2be52329bac0e6811366f69f29faeaf28c
Mario Molina <mm2535@cornell.edu>
2019-08-16 13:49:53 -0400

    create run function for RF

--- python_code/functions_RF.py
+++ python_code/functions_RF.py
@@ -2,6 +2,7 @@
 import csv, random, os, re, gc
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
