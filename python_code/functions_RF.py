5d64f847b9f6a3ad5d6f39c57b27d9351b0716ba
Mario Molina <mm2535@cornell.edu>
2019-08-18 17:46:11 -0400

    adjust parameters

--- python_code/functions_RF.py
+++ python_code/functions_RF.py
@@ -94,7 +94,7 @@ def get_features(file):
                               "weather_vars": weather_vars }
     else:
         # rename weather variables
-        weather_vars = { weather_keys[x]: [ weather_vars[x] + '_t' + str(i) for i in range(5) ] for x in range(len(weather_vars)) }
+        weather_vars = { weather_keys[x]: [ weather_vars[x] + '_' + str(i) for i in range(5) ] for x in range(len(weather_vars)) }
 
         # rename time_varying variables
         features_sociodem_varying = [ name + "_" + str(i) for i in range(5) for name in features_sociodem_varying ]
